2bd04db36f954d60cffd5895e89db506d1546d88
amanfangeria980 <amanfangeria980@gmail.com>
2024-01-14 23:27:16 +0530

    chatup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useMemo, useState } from 'react'
+import {io} from 'socket.io-client'
+import {Container, Typography, TextField, Button, Box, Stack} from '@mui/material'
+const App = () => {
+  // Initialisation of socket
+  const socket = useMemo(()=>{
+    return io(import.meta.env.VITE_BACKEND_URL,{
+      withCredentials:true,
+      transports : ['websocket']
+    })
+  },[]);
+
+  const [message,setMessage]=useState("");
+  const [room,setRoom]=useState("");
+  const [socketID,setSocketID]=useState("");
+  const [messages,setMessages]=useState([]);
+  const[roomName,setRoomName]=useState("");
+  const handleSumbit=(e)=>{
+    e.preventDefault();
+    socket.emit("message",{message,room});
+    setMessage("");
+    setRoom("");
+  }
+
+  const joinRoomHandler=(e)=>{
+    e.preventDefault();
+    socket.emit("join-room",roomName);
+    setRoomName("")
+  }
+
+  // console.log(messages);
+
+  useEffect(()=>{
+
+    // Events
+    socket.on("connect",()=>{
+      console.log("connected",socket.id)
+      setSocketID(socket.id);
+    })
+    socket.on('welcome',(msg)=>{
+      console.log(msg);     
+    })
+    socket.on("receive-message",(data)=>{
+      console.log(data);
+      setMessages((messages)=>[...messages,data]);
+    })
+    return()=>{
+      socket.disconnect();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  return (
+    <Container>
+      <Box sx={{height: 500}}/>
+      <Typography variant="h3" component="div" gutterBottom>
+        Welcome to ChatUp
+      </Typography>
+      <Typography variant="h5" component="div" gutterBottom>
+        {socketID}
+      </Typography>
+
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
+        
+        <TextField
+          id="outined-basic"
+          label="Room Name" value={roomName} onChange={(e)=>setRoomName(e.target.value)}
+        />
+
+        <Button variant="contained" color="primary" type='submit'>
+          Join
+        </Button>
+      </form>
+
+
+      <form onSubmit={handleSumbit}>
+        <TextField
+          id="outined-basic"
+          label="Message" value={message} onChange={(e)=>setMessage(e.target.value)}
+        />
+        <TextField
+          id="outined-basic"
+          label="Room" value={room} onChange={(e)=>setRoom(e.target.value)}
+        />
+        <Button variant="contained" color="primary" type='submit'>
+          Send
+        </Button>
+      </form>
+
+      <Stack>
+      {
+        messages.map((msg,i)=>{
+          return <Typography key={i} variant="h6" component="div" gutterBottom>
+            {msg}
+        </Typography>
+        })
+      }
+      </Stack>
+    </Container>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,77 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { Server } from 'socket.io';
+dotenv.config();
+import { createServer } from 'http';
+import cors from 'cors'
+import jwt from 'jsonwebtoken'; 
+import cookieParser from 'cookie-parser';
+
+const app=express();
+const port=process.env.PORT || 3000;
+const secretKeyJWT="afjsfdasklklafklj"
+
+// Initialisation of socket
+const server=createServer(app);
+const io=new Server(server,{
+    cors: {
+        origin: "*",
+        methods: ["GET","POST"],
+        credentials: true
+    }
+})
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
+
+app.get("/login",(req,res)=>{
+    const token=jwt.sign({_id:"asdfdfallkdlkfkl"},secretKeyJWT)
+    res.cookie("token",token,{httpOnly: true,secure: true,sameSite:"none"}).json({
+        message: "Login Success"
+    })
+})
+
+// Socket middlware
+// Go to localhost:3000/login and token is stored in the cookie then hit localhost:5173 for the client socket to be connected to the circuit 
+// io.use((socket,next)=>{
+//     cookieParser()(socket.request,socket.request.res,(err)=>{
+//         if(err)return next(err);
+//         const token=socket.request.cookies.token;
+//         if(!token)return next(new Error("Authentication Error"))
+//         const decoded=jwt.verify(token,secretKeyJWT);
+//         next();
+//     })
+// })
+
+// Events
+io.on("connection", (socket) => {
+    console.log("User Connected",socket.id);
+    
+    // socket.emit('welcome','Welcome to the server')
+    // socket.broadcast.emit('welcome',socket.id+" joined the server.")
+    socket.on('disconnect',()=>{
+        console.log("User Disconnected", socket.id)
+    })
+
+    socket.on("message",({room,message})=>{
+        // console.log(data);
+        if(room===""){
+            socket.broadcast.emit("receive-message",message);
+        }
+        else{
+            // can use io.to or socket.to here as we are sending to message from one individualt to another(one to one private messge)
+            socket.to(room).emit("receive-message",message);
+        }
+    })
+
+    socket.on("join-room",(room)=>{
+        socket.join(room);
+        console.log(`User ${socket.id} joined the room ${room}`);
+    })
+});
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
